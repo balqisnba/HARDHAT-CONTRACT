9fbb8a32aa93c1155eeae2d42b2d5940b76917f7
qurratulainan <qrrtlainan88@gmail.com>
2025-01-18 01:25:37 +0800

    header dah dibuat tapi nanti webpage akan nampak blank

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,31 +1,38 @@
-import "./App.css";
+import "./interface.css";
 import Button from "./components/Button";
-import { useNavigate } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
 
-function App() {
+const App = () => {
   const navigate = useNavigate();
 
   return (
     <div className="homepage-container">
-      <div className="heading-container">
-        <h1 className="main-title">Welcome to the Voting Portal!</h1>
-        <p className="sub-title">Please choose an option below to proceed.</p>
-      </div>
+      <div className="image-container">
+        {/* Left Image Section */}
+        <div className="image-container">
+          <img src="/src/assets/seventeen.png" alt="Placeholder" className="left-image" />
+        </div>
 
-      <div className="button-container">
-        <Button
-          text="Register"
-          onClick={() => navigate("/register")}
-          className="btn-register"
-        />
-        <Button
-          text="Login"
-          onClick={() => navigate("/login")}
-          className="btn-login"
-        />
+        {/* Right Content Section */}
+        <div className="text-button-container">
+          <h1 className="main-title">WELCOME TO VOTING PORTAL</h1>
+          <p className="sub-title">Please choose an option below to proceed.</p>
+          <div className="button-container">
+            <Button
+              text="Register"
+              onClick={() => navigate("/register")}
+              className="btn-register"
+            />
+            <Button
+              text="Login"
+              onClick={() => navigate("/login")}
+              className="btn-login"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Results.tsx
@@ -0,0 +1,10 @@
+
+const Results = () => {
+    return (
+        <div>
+            <h1>Election Results</h1>
+        </div>
+    );
+};
+
+export default Results;
