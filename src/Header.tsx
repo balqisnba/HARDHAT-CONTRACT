c5c3e2767f639c027948cf91ec3ffe7844a2cfc9
qurratulainan <qrrtlainan88@gmail.com>
2025-01-18 11:09:55 +0800

    try header

--- src/Header.tsx
+++ src/Header.tsx
@@ -10,42 +10,27 @@ const Header = () => {
             <nav className="nav-links">
                 <ul>
                     <li>
-                        <NavLink
-                            to="/src/pages/Home.tsx"
-                            className={({ isActive }) => (isActive ? "active" : "")}
-                        >
+                        <NavLink to="/home" className={({ isActive }) => (isActive ? "active" : "")}>
                             Home
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink
-                            to="/profile"
-                            className={({ isActive }) => (isActive ? "active" : "")}
-                        >
+                        <NavLink to="/profile" className={({ isActive }) => (isActive ? "active" : "")}>
                             Profile
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink
-                            to="/vote"
-                            className={({ isActive }) => (isActive ? "active" : "")}
-                        >
+                        <NavLink to="/vote" className={({ isActive }) => (isActive ? "active" : "")}>
                             Vote
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink
-                            to="/results"
-                            className={({ isActive }) => (isActive ? "active" : "")}
-                        >
+                        <NavLink to="/results" className={({ isActive }) => (isActive ? "active" : "")}>
                             Results
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink
-                            to="/contact-us"
-                            className={({ isActive }) => (isActive ? "active" : "")}
-                        >
+                        <NavLink to="/contact-us" className={({ isActive }) => (isActive ? "active" : "")}>
                             Contact Us
                         </NavLink>
                     </li>
